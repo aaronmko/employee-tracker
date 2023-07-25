85d9651c231e2d1a3b400d53f04810dd87c0b5fc
minkiko@gmail.com <minkiko@gmail.com>
2023-07-24 22:04:30 -0500

    prompt structure startQUestion

--- index.js
+++ index.js
@@ -24,3 +24,12 @@ const connection = mysql.createConnection({
       startPrompt();
     });
   });
+
+  function startPrompt() {
+    const startQuestion = [{
+      type: "list",
+      name: "action",
+      message: "what would you like to do?",
+      loop: false,
+      choices: ["View all employees", "View all roles", "View all departments", "add an employee", "add a role", "add a department", "update role for an employee", "update employee's manager", "view employees by manager", "delete a department", "delete a role", "delete an employee", "View the total utilized budget of a department", "quit"]
+    }]
